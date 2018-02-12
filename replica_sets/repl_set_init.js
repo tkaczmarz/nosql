6c54a28a213a9c7135f123085c0cf62f092672d2
Wlodek Bzyl <matwb@univ.gda.pl>
2018-02-12 21:08:08 +0100

    Add files to replica_set folder

---
+++ replica_sets/repl_set_init.js
@@ -0,0 +1,12 @@
+cfg = {
+  _id: "abc",
+  members: [
+    { _id: 0, host: "localhost:27001" },
+    { _id: 1, host: "localhost:27002" },
+    { _id: 2, host: "localhost:27003" }
+  ]
+}
+rs.help()
+
+rs.initiate(cfg)
+
