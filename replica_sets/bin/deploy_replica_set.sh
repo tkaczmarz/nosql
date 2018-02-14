36808cf367eaf201540144fd0a3313092dfb83a2
Wlodek Bzyl <matwb@univ.gda.pl>
2018-02-14 11:36:02 +0100

    Update

--- replica_sets/bin/deploy_replica_set.sh
+++ replica_sets/bin/deploy_replica_set.sh
@@ -20,24 +20,24 @@ if [ $# -eq 0 ]; then
 fi
 
 name=$1
-dbpath=$1/data
-logpath=$1/log
+dbpath=$name/data
+logpath=$name/log
+pid=$name/pid
 
-mkdir -p $dbpath-{1,2,3} $logpath-{1,2,3}
+mkdir -p $dbpath-{1,2,3}
+# touch $pid-{1,2,3}
 
-pid=$1/pid
-
-mongod --port 27001 --replSet $name \
-  --dbpath $dbpath-1 --logpath $path-1 --logappend  \
-  --pidfilepath $pid-1 --fork \
+echo mongod --port 27001 --replSet $name \
+  --dbpath ${dbpath}-1 --logpath ${logpath}-1.log --logappend  \
+  --pidfilepath ${pid}-1 \
   --oplogSize 50 --smallfiles
 
-mongod --port 27002 --replSet $name \
-  --dbpath $dbpath-2 --logpath $path-2 --logappend  \
-  --pidfilepath $pid-2 --fork \
-  --oplogSize 50 --smallfiles
-
-mongod --port 27003 --replSet $name \
-  --dbpath $dbpath-3 --logpath $path-3 --logappend  \
-  --pidfilepath $pid-3 --fork \
-  --oplogSize 50 --smallfiles
+# mongod --port 27002 --replSet $name \
+#   --dbpath $dbpath-2 --logpath $path-2 --logappend  \
+#   --pidfilepath $pid-2 --fork \
+#   --oplogSize 50 --smallfiles
+#
+# mongod --port 27003 --replSet $name \
+#   --dbpath $dbpath-3 --logpath $path-3 --logappend  \
+#   --pidfilepath $pid-3 --fork \
+#   --oplogSize 50 --smallfiles
