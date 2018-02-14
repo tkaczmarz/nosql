f5ac1f0587f5c39220184588ff862c37bccbe59d
Wlodek Bzyl <matwb@univ.gda.pl>
2018-02-14 12:53:58 +0100

    Update

--- replica_sets/bin/stop_replica_set.sh
+++ replica_sets/bin/stop_replica_set.sh
@@ -5,14 +5,20 @@ cat << EOF >&2
 
 Stop replica set created with the bin/deploy_replica_set.sh script.
 
+Usage:
+    $(basename $0) REPLICA_SET_NAME
+
+Example:
+    bin/$(basename $0) carbon
+
 EOF
 }
 
-if [ $# -gt 0 ]; then
+if [[ "$1" == "--help" || "$1" == "-help" || "$1" == "-h" ]]; then
   usage
   exit 0
 fi
 
-kill -2 $(cat /tmp/mongod-carbon-1.lock)
-kill -2 $(cat /tmp/mongod-carbon-2.lock)
-kill -2 $(cat /tmp/mongod-carbon-3.lock)
+echo kill -2 $(cat /tmp/mongod-$1-1.lock)
+echo kill -2 $(cat /tmp/mongod-$1-2.lock)
+echo kill -2 $(cat /tmp/mongod-$1-3.lock)
