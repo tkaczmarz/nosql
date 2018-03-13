87d2d823f4a55fbdc30e16edf6c507e730e5818b
Wlodek Bzyl <matwb@univ.gda.pl>
2018-03-13 16:11:51 +0100

    Update

--- docker/bin/get_geospatial_data.sh
+++ docker/bin/get_geospatial_data.sh
@@ -4,6 +4,6 @@ neighborhoods='https://raw.githubusercontent.com/mongodb/docs-assets/geospatial/
 restaurants='https://raw.githubusercontent.com/mongodb/docs-assets/geospatial/restaurants.json'
 zipcodes='http://media.mongodb.org/zips.json'
 
-curl -s $neighborhoods | gzip --stdout > neighborhoods.json.gz
-curl -s $restaurants   | gzip --stdout > restaurants.json.gz
-curl -s $zipcodes   | gzip --stdout > zipcodes.json.gz
+curl -s $neighborhoods | gzip --stdout > datasets/neighborhoods.json.gz
+curl -s $restaurants   | gzip --stdout > datasets/restaurants.json.gz
+curl -s $zipcodes      | gzip --stdout > datasets/zipcodes.json.gz
